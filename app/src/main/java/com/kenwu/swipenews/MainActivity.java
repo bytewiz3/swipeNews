136dba066ba25de9c1e08f51a3a76e76bf10bfe9
Ken <jw2585@cornell.edu>
2020-05-20 16:39:17 -0400

    Updated MainActivity

--- app/src/main/java/com/kenwu/swipenews/MainActivity.java
+++ app/src/main/java/com/kenwu/swipenews/MainActivity.java
@@ -31,8 +31,6 @@ public class MainActivity extends AppCompatActivity {
         navController = navHostFragment.getNavController();
         NavigationUI.setupWithNavController(navView, navController);
         //NavigationUI.setupActionBarWithNavController(this, navController);
-
-
     }
 
     @Override
