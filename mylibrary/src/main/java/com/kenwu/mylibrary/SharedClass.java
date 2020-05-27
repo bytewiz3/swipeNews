93a56fe259f339b1b7c717c2491efa42bb1d5080
Ken <jw2585@cornell.edu>
2020-05-27 15:22:24 -0400

    SignUp LogIn

---
+++ mylibrary/src/main/java/com/kenwu/mylibrary/SharedClass.java
@@ -0,0 +1,16 @@
+package com.kenwu.mylibrary;
+
+public final class SharedClass {
+    public static String ROOT_UID = "";
+
+    public static final String Name = "keyName";
+    public static final String Address = "keyAddress";
+    public static final String Mail = "keyMail";
+    public static final String Photo = "keyPhoto";
+    public static final String Phone = "keyPhone";
+    public static final String CameraOpen = "keyCameraDialog";
+
+    public static final int PERMISSION_GALLERY_REQUEST = 1;
+
+    public static final String USER_PATH = "/users";
+}
